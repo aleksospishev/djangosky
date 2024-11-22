213b3a7a0c9570869d7acf3395343baf31e6968a
aleksandr <aleksospishev@yandex.ru>
2024-11-22 22:08:38 +0300

    add static in url

--- djangosky/urls.py
+++ djangosky/urls.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("catalog.urls", namespace="catalog"))]
+
+urlpatterns = [path("admin/", admin.site.urls),
+               path("", include("catalog.urls", namespace="catalog"))
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
